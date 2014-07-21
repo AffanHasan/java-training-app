a38243edfba29f08fca02c5b0031e9a960b7c306
affan.hasan <affan.hasan@royalcyber.com>
2014-07-21 09:51:10 +0500

    Added support for HTTP Servlets in pom.xml file

---
+++ src/main/java/org/saeen/java/training/controller/TopicCRUDServlet.java
@@ -0,0 +1,15 @@
+package org.saeen.java.training.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/topicCRUD")
+public class TopicCRUDServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1112089948529142653L;
+
+	
+}
